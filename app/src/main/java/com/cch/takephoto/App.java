13909b425ecd337a40804a6530c3a8d3da657177
Fstar <568486442@qq.com>
2017-09-13 15:08:52 +0800

    no msg

--- app/src/main/java/com/cch/takephoto/App.java
+++ app/src/main/java/com/cch/takephoto/App.java
@@ -1,11 +1,14 @@
 package com.cch.takephoto;
 
 import android.app.Application;
+import android.os.Environment;
 
 import com.google.android.cameraview.takephoto.TakePhotoUtils;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.io.File;
 
 /**
  * Created by Fstar on 2017/9/6.
@@ -15,7 +18,7 @@ public class App extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-        TakePhotoUtils.init(getApplicationContext(),"");
+        TakePhotoUtils.init(getApplicationContext(),createCachePath());
         initImageLoader();
     }
 
@@ -36,5 +39,21 @@ public class App extends Application{
 //                .writeDebugLogs()
                 .build();
         ImageLoader.getInstance().init(conf);
+    }
+
+    private String createCachePath() {
+        String filePath = null;
+        String foder = "image";
+        boolean hasSDCard = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        if (hasSDCard) { // SD卡根目录
+            filePath = Environment.getExternalStorageDirectory().toString() + File.separator+foder+File.separator;
+        } else {  // 系统下载缓存根目录
+            filePath = Environment.getDownloadCacheDirectory().toString() + File.separator+foder+File.separator;
+        }
+        File f1 = new File(filePath);
+        if (!f1.exists()) {
+            f1.mkdirs();
+        }
+        return filePath;
     }
 }
